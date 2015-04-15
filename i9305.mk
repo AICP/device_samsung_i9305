eb695a6333aa42d564932f8e837b7bea307cc2a3
doc <doc.divxm@gmail.com>
2015-04-15 13:15:28 +0300

    i9305: More fixes for AICP
    Change-Id: Ic72f7aedf720cd8957e9743f4336a7095e6ebce3

--- i9305.mk
+++ i9305.mk
@@ -74,8 +74,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     com.android.nfc_extras
 
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
 # RIL
 PRODUCT_PROPERTY_OVERRIDES += \
       mobiledata.interfaces=pdp0,gprs,ppp0,rmnet0,rmnet1
